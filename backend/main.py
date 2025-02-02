dfbd94279674f403951c7a1c45ab41bc65a760c6
dani-ribeiro <10DanielRibeiro@gmail.com>
2025-02-02 01:34:20 -0600

    Stock Search Autocomplete (NASDAQ)

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
@@ -7,8 +8,15 @@ import time
 load_dotenv()
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
-# Connect to MongoDB
+# MongoDB connection
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client["stock_dashboard"]
 
@@ -18,37 +26,56 @@ def read_root():
 
 # --- /search endpoint ---
 assets = db["assets"]   # db collection w/ basic asset information
-# assets doesn't change, so we can cache it for a day
-cache = None
-cache_timestamp = 0
-CACHE_EXPIRATION_TIME = 86400 # 1 day
+# assets don't change, so we can cache it for a day
+cache = {}
+CACHE_EXPIRATION_TIME = 86400   # 1 day = 86400 seconds
 
 @app.get("/search")
-async def get_assets():
-    """ gets basic info from all assets in DB --> displayed in search bar autocomplete """
-    global cache, cache_timestamp
+async def get_assets(query: str = Query(None, min_length=1)):
+    """ returns basic info for assets that match the search bar query """
+    global cache
 
     current_time = time.time()
 
-    if cache and (current_time - cache_timestamp) < CACHE_EXPIRATION_TIME:
-        return cache
+    # if cached data is available and not expired, return the cache. Otherwise, query the DB
+    if query in cache and (current_time - cache[query]["timestamp"]) < CACHE_EXPIRATION_TIME:
+        return cache[query]["data"]
 
-    assets_cursor = assets.find({}, {"_id": 0, "Ticker": 1, "Name": 1, "Exchange": 1, "Flag": 1, "Country": 1})
-    
-    assets_dict = []
-    for asset in assets_cursor:
-        assets_dict.append({
+    query = query.lower()
+
+    # query DB based on prefix
+    prefix_query = {
+        "$or": [
+            {"Ticker": {"$regex": f"^{query}", "$options": "i"}},
+            {"Name": {"$regex": f"^{query}", "$options": "i"}},
+            {"Exchange": {"$regex": f"^{query}", "$options": "i"}},
+            {"Country": {"$regex": f"^{query}", "$options": "i"}},
+        ]
+    }
+
+    assets_cursor = assets.find(
+        prefix_query,
+        {"_id": 0, "Ticker": 1, "IconURL": 1, "Name": 1, "Country": 1, "Exchange": 1, "CountryFlag": 1}
+    ).sort([("Ticker", 1), ("Name", 1)])
+
+    assets_list = [
+        {
             "ticker": asset.get("Ticker", ""),
+            "icon": asset.get("IconURL", ""),
             "full_name": asset.get("Name", ""),
             "market": asset.get("Exchange", ""),
             "country": asset.get("Country", ""),
-            "country_flag": asset.get("Flag", "")
-        })
+            "country_flag": asset.get("CountryFlag", ""),
+        }
+        for asset in assets_cursor
+    ]
 
-    cache = assets_dict
-    cache_timestamp = current_time
+    cache[query] = {
+        "data": assets_list,
+        "timestamp": current_time
+    }
 
-    return cache
+    return assets_list
 
 if __name__ == "__main__":
     import uvicorn
--- frontend/app/dashboard/components/sidebar/searchbar.tsx
+++ frontend/app/dashboard/components/sidebar/searchbar.tsx
@@ -13,31 +13,44 @@ import { useSidebar } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-// hardcoded assets
-const assets = [
-  {
-    icon: "https://s3-symbol-logo.tradingview.com/nvidia.svg",
-    ticker: "NVDA",
-    fullName: "NVIDIA Corporation",
-    market: "NASDAQ",
-    country: "US",
-    countryFlag: "https://s3-symbol-logo.tradingview.com/country/US.svg",
-  },
-  {
-    icon: "https://s3-symbol-logo.tradingview.com/apple.svg",
-    ticker: "AAPL",
-    fullName: "Apple Inc.",
-    market: "NASDAQ",
-    country: "US",
-    countryFlag: "https://s3-symbol-logo.tradingview.com/country/US.svg",
-  },
-];
+interface Asset {
+  ticker: string;
+  icon: string;
+  full_name: string;
+  market: string;
+  country: string;
+  country_flag: string;
+}
 
 export function SearchBar() {
-  const [commandOpen, setCommandOpen] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
-
+  const [commandOpen, setCommandOpen] = useState<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [assets, setAssets] = useState<Asset[]>([]);
   const { state } = useSidebar();
+
+  // autocomplete search results
+  useEffect(() => {
+    if (searchQuery.length < 1) return;
+
+    const fetchAssets = async () => {
+      try {
+        const response = await fetch(
+          `http://127.0.0.1:8000/search?query=${searchQuery}`
+        );
+        const data = await response.json();
+        setAssets(data);
+      } catch (error) {
+        console.error("Error fetching assets:", error);
+      }
+    };
+
+    // Debounce the API request
+    const debounceTimer = setTimeout(() => {
+      fetchAssets();
+    }, 100); // Adjust the delay as needed
+
+    return () => clearTimeout(debounceTimer);
+  }, [searchQuery]);
 
   // search keybind (cmd + k, ctrl + k)
   useEffect(() => {
@@ -50,14 +63,6 @@ export function SearchBar() {
     document.addEventListener("keydown", keypress);
     return () => document.removeEventListener("keydown", keypress);
   }, []);
-
-  // filter search assets by ticker, fullname, or market
-  const filteredAssets = assets.filter(
-    (asset) =>
-      asset.ticker.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      asset.fullName.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      asset.market.toLowerCase().includes(searchQuery.toLowerCase())
-  );
 
   return (
     <div
@@ -72,7 +77,6 @@ export function SearchBar() {
           placeholder={"Search"}
           icon={<Search className="w-5 h-5 text-muted-foreground" />}
           onClick={() => setCommandOpen(true)}
-          readOnly
         />
         <kbd className="pointer-events-none absolute right-3 top-1/2 transform -translate-y-1/2 inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">⌘</span>K
@@ -86,50 +90,53 @@ export function SearchBar() {
           value={searchQuery}
           onValueChange={setSearchQuery}
         />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Securities">
-            {filteredAssets.map((asset) => (
-              <CommandItem key={asset.ticker} className="p-2">
-                <div className="grid grid-cols-6 gap-4 items-center">
-                  {/* asset's icon & ticker */}
-                  <div className="flex items-center justify-center">
-                    <Image
-                      src={asset.icon}
-                      alt={asset.ticker}
-                      width={28}
-                      height={28}
-                      className="rounded-full"
-                    />
-                    <span className="ml-3 font-medium">{asset.ticker}</span>
-                  </div>
+        <CommandList className="min-h-[300px]">
+          {assets.length === 0 ? (
+            <CommandEmpty>No results found.</CommandEmpty>
+          ) : (
+            <CommandGroup heading="Securities">
+              {assets.slice(0, 100).map((asset: Asset) => (
+                <CommandItem key={asset.ticker} className="p-2">
+                  <div className="grid grid-cols-6 gap-4 items-center">
+                    {/* asset's icon & ticker */}
+                    <div className="flex items-center justify-start">
+                      <Image
+                        src={asset.icon}
+                        alt={asset.ticker}
+                        width={28}
+                        height={28}
+                        className="rounded-full bg-black object-contain p-[3.5px]"
+                      />
+                      <span className="ml-3 font-medium">{asset.ticker}</span>
+                    </div>
 
-                  {/* asset's official name */}
-                  <div className="col-span-4">
-                    <div className="font-medium">
-                      {asset.fullName.length > 38
-                        ? `${asset.fullName.slice(0, 38)}...`
-                        : asset.fullName}
+                    {/* asset's official name */}
+                    <div className="col-span-4 ml-5">
+                      <div className="font-medium">
+                        {asset.full_name.length > 30
+                          ? `${asset.full_name.slice(0, 30)}...`
+                          : asset.full_name}
+                      </div>
+                    </div>
+
+                    {/* asset's market & flag */}
+                    <div className="flex items-center justify-end col-span-1 space-x-2">
+                      <span className="mr-3 text-xs text-muted-foreground">
+                        {asset.market}
+                      </span>
+                      <Image
+                        src={asset.country_flag}
+                        alt={asset.country}
+                        width={20}
+                        height={20}
+                        className="rounded-full"
+                      />
                     </div>
                   </div>
-
-                  {/* asset's market & flag */}
-                  <div className="flex items-center justify-end col-span-1 space-x-2">
-                    <span className="mr-3 text-xs text-muted-foreground">
-                      {asset.market}
-                    </span>
-                    <Image
-                      src={asset.countryFlag}
-                      alt={asset.country}
-                      width={20}
-                      height={20}
-                      className="rounded-full"
-                    />
-                  </div>
-                </div>
-              </CommandItem>
-            ))}
-          </CommandGroup>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          )}
         </CommandList>
       </CommandDialog>
     </div>
